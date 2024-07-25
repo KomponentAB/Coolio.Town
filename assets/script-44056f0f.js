269d8d1b51a24973b50c51ae850b5113e4365ac3
klinshy <79143155+klinshy@users.noreply.github.com>
2024-07-25 08:47:24 +0000

    Deploying to gh-pages from master @ 6064273d261a4cce52004fe39dba32c6e49900c7 🚀

--- assets/script-44056f0f.js
+++
@@ -1,2 +0,0 @@
-const t="https://apps.taskmagic.com/api/v1/webhooks/Wn8CdqSXOlSSMewy6xL60";(function a(){WA.onInit().then(()=>{const e=WA.player.id,o=WA.player.name;fetch(t,{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify({id:e,name:o})}),setTimeout(a,6e4)})})();
-//# sourceMappingURL=script-44056f0f.js.map
